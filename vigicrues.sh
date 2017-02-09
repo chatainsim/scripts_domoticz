9c0845a7f64ef1fb3af3608cb4a443c4cfc74daf
Simon <chatainsim@gmail.com>
2017-02-09 12:21:38 +0100

    Create vigicrues.sh

---
+++ vigicrues.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+### PARAMETRES A MODIFIER
+#Domoticz
+SERVER="192.168.1.254:3434"
+#ID Grenoble Bastille
+STATION="W141001001"
+#IDX Hauteur eau
+HIDX="215"
+#IDX Vitesse eau
+SIDX="216"
+### FIN DES PARAMETERES A MODIFIER
+
+
+#Url niveau eau
+URLNIVEAU="http://www.vigicrues.gouv.fr/niveau3.php?CdStationHydro=$STATION&typegraphe=h&AffProfondeur=24&nbrstations=3&ong=2"
+NIVEAU=$(curl -s "$URLNIVEAU" | grep titre_cadre | awk -F ">|<" '{print $31}')
+#Url debit eau
+URLDEBIT="http://www.vigicrues.gouv.fr/niveau3.php?CdStationHydro=$STATION&typegraphe=q&AffProfondeur=24&nbrstations=3&ong=2"
+DEBIT=$(curl -s "$URLDEBIT" | grep titre_cadre | awk -F ">|<" '{print $31}')
+
+URLHAUTEUR="http://$SERVER/json.htm?type=command&param=udevice&idx=$HIDX&svalue=$NIVEAU"
+URLSPEED="http://$SERVER/json.htm?type=command&param=udevice&idx=$SIDX&svalue=$DEBIT"
+curl -s $URLHAUTEUR > /dev/null
+curl -s $URLSPEED > /dev/null
