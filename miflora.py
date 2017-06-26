86c386bf721bc55551605cc4213d9366bbc436c6
Simon <chatainsim@gmail.com>
2017-06-26 14:35:04 +0200

    Update miflora.py
    Check value to prevent a bug

--- miflora.py
+++ miflora.py
@@ -15,13 +15,19 @@ poller = MiFloraPoller("C4:7C:8D:60:C1:2B")
 maintenant = datetime.now()
 date = "{0}/{1}/{2} - {3}:{4}".format(maintenant.day, maintenant.month, maintenant.year, maintenant.hour, maintenant.minute)
 
-
-
-
 temp = poller.parameter_value("temperature")
 hum = poller.parameter_value(MI_MOISTURE)
 bat = poller.parameter_value(MI_BATTERY)
 fert = poller.parameter_value(MI_CONDUCTIVITY)
+
+# Si la temperature est au dessus de 50 degres ou l'humiditee en dessus de 100 ou engrais au dessus de 1000 alors on ignore
+# C'est pour la cas ou il y est un bug dans le dialog en bluetooth.
+if temp > 50:
+        exit(1)
+if hum > 100:
+        exit(1)
+if fert > 1000:
+        exit(1)
 
 url = "http://192.168.1.254:3434/json.htm?type=command&param=udevice&idx={0}&nvalue=0&svalue={1};{2};0&battery={3}&rssi=10".format(idx, temp, hum, bat)
 r = requests.get(url)
